0f4b4cd7d5cbd61869d73577bb3471de06e7c300
adelekanley <49572272+adelekanley@users.noreply.github.com>
2020-06-18 19:41:27 -0500

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,5 +4,6 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
     return HttpResponse("Hello, world. This is Sean's message.")
     return HttpResponse("Hello, world. This is Nataliya's message.")
+   return HttpResponse("Hello, world. This is Adele's message.")
 
 # Create your views here.
