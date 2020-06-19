e3c00394fa627fbe07632348615d38b4ba3a9d26
nzhuromskaya <46686968+nzhuromskaya@users.noreply.github.com>
2020-06-18 19:38:00 -0500

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
     return HttpResponse("Hello, world. This is Sean's message.")
-    return HttpResponse("Hello, world. This is Nataliya's message.")
+    
 
 # Create your views here.
